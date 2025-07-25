6f1337b204a5eefe9c6dff562d083ef089512f42
Ravi-gurugram <125189351+Ravi-gurugram@users.noreply.github.com>
2025-07-25 11:42:32 +0530

    [1748] [Time Beats: 100.00%] [Memory Beats: 18.49%] - LeetPush

---
+++ Sum-of-Unique-Elements.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        
+        unordered_map<int,int>m;
+        for(int i=0;i<nums.size();i++)
+            m[nums[i]]++ ;
+        
+        int sum=0;
+        for( auto it=m.begin();it!=m.end();it++)
+             if(it->second==1)
+                sum+=it->first ;
+
+return sum ;
+
+    }
+};
